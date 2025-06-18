c46456918be1221e6f47eb1c2c3bea2f31e2be76
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-06-18 15:12:11 +0200

    Provides clients url to handle CORS requirements.

--- server/app.ts
+++ server/app.ts
@@ -6,7 +6,7 @@ import { errorHandler } from './middleware/errorHandler';
 import { CountryServiceError } from './utils/CountryServiceError';
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.get('/', getRootMessage);
