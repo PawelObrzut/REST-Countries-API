a1bf82ed61eb3eb97bbedd0c3f1ba735981fb6c1
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-06-25 19:49:32 +0200

    Modifies payload response so that border are full names (not alpha3code)

--- server/services/fetchCountryByName.ts
+++ server/services/fetchCountryByName.ts
@@ -1,29 +1,59 @@
-import DATA from '../database/data.json'
-import { keyFilter } from '../utils/keyFilter'
+import DATA from "../database/data.json";
+import { keyFilter } from "../utils/keyFilter";
 
 type Language = {
-  iso639_1: string,
-  iso639_2: string,
-  name: string,
-  nativeName: string,
-}
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+};
 
 type Currency = {
-  code: string,
-  name: string,
-  symbol: string
-}
+  code: string;
+  name: string;
+  symbol: string;
+};
 
 export const fetchCountryByName = async (name: string) => {
-  const theCountry = await DATA.find(country => country.name === name)
+  const theCountry = await DATA.find((country) => country.name === name);
 
   if (!theCountry) return null;
 
-  const result = keyFilter(theCountry, ['flag', 'name', 'nativeName', 'topLevelDomain', 'population', 'region', 'subregion', 'capital', 'domain', 'currencies', 'languages', 'borders']);
+  const result = keyFilter(theCountry, [
+    "flag",
+    "name",
+    "nativeName",
+    "topLevelDomain",
+    "population",
+    "region",
+    "subregion",
+    "capital",
+    "domain",
+    "currencies",
+    "languages",
+    "borders",
+  ]);
+
+  if (!result.borders) {
+    return {
+      ...result,
+      borders: [],
+      currencies: (result.currencies && result.currencies.map((currency: Currency) => currency.name)) || [""],
+      languages: (result.languages && result.languages.map((language: Language) => language.name)) || [""],
+    };
+  }
+
+  const fullBorderNames = result.borders.map((alpha3Code: string) => {
+    const fullBorderName = DATA.find((country) => country.alpha3Code === alpha3Code);
+    return fullBorderName?.name;
+  });
 
   return {
     ...result,
-    currencies: result.currencies && result.currencies.map((currency: Currency) => currency.name) || [''],
-    languages: result.languages && result.languages.map((language: Language) => language.name) || ['']
+    borders: fullBorderNames,
+    currencies: (result.currencies &&
+      result.currencies.map((currency: Currency) => currency.name)) || [""],
+    languages: (result.languages &&
+      result.languages.map((language: Language) => language.name)) || [""],
   };
-}
+};
