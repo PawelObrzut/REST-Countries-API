49ec9511ab8cdb97add6e7305fa0af65c169ce37
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-12-10 15:28:37 +0100

    adds process.env prior to deployment

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import app from './app'
 
-app.listen(3000, () => {
-  console.log('Server up and runninng at port 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
